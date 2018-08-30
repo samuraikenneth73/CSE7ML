7083c4f6ad4333a3f9cda38270cf7290f188b7ec
Surya Xanden <26842395+SuryaXanden@users.noreply.github.com>
2018-08-30 16:07:50 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -3,34 +3,34 @@ For a given set of training data examples stored in a .CSV file, implement and d
 '''
 import numpy as np
 import pandas as pd
-data=pd.DataFrame(data=pd.read_csv('p2.csv'))
-con=np.array(data.iloc[:,0:-1])
-tar=np.array(data.iloc[:,-1])
+data = pd.DataFrame(data = pd.read_csv('p2.csv'))
+con = np.array(data.iloc[:,0:-1])
+tar = np.array(data.iloc[:,-1])
 def learn(con,tar):
-    sh=con[0].copy()
+    sh = con[0].copy()
     print("Initialization of specific hypothesis and general hypothesis")
     print(sh)
-    gh=[["?"for i in range(len(sh))] for i in range(len(sh))] 
+    gh = [["?"for i in range(len(sh))] for i in range(len(sh))] 
     print(gh)
     for i,h in enumerate(con):
-        if tar[i]=="YES":
+        if tar[i] == "YES":
             for x in range(len(sh)):
-                if h[x]!=sh[x]:
-                    sh[x]='?'
-                    gh[x][x]='?'
-        if tar[i]=="NO":
+                if h[x] != sh[x]:
+                    sh[x] = '?'
+                    gh[x][x] = '?'
+        if tar[i] == "NO":
             for x in range(len(sh)):
-                if h[x]!=sh[x]:
-                    gh[x][x]=sh[x]
+                if h[x] != sh[x]:
+                    gh[x][x] = sh[x]
 		else:
-		    gh[x][x]='?'
+		    gh[x][x] = '?'
         print("steps of candidate eliminate algorithm",i+1)
         print(sh)
         print(gh)
-    indices=[i for i,val in enumerate(gh)if val==['?','?','?','?','?','?']]
+    indices=[i for i,val in enumerate(gh)if val == ['?','?','?','?','?','?']]
     for i in indices:
         gh.remove(['?','?','?','?','?','?'])
     return sh,gh
-s,g=learn(con,tar)
+s,g = learn(con,tar)
 print("Final sh",s)
 print("Final gh",g)
