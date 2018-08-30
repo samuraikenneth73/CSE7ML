f89cdae2d76652741aebde0ecf2cb51ab977de4c
Surya Xanden <26842395+SuryaXanden@users.noreply.github.com>
2018-08-30 16:42:09 +0530

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,9 +1,6 @@
-'''
-For a given set of training data examples stored in a .CSV file, implement and demonstrate the Candidate-Elimination algorithmto output a description of the set of all hypotheses consistent with the training examples.
-'''
 import numpy as np
 import pandas as pd
-data = pd.DataFrame(data = pd.read_csv('p2.csv'))
+data = pd.read_csv('p2.csv')#, na_values='', skiprows=3)
 con = np.array(data.iloc[:,0:-1])
 tar = np.array(data.iloc[:,-1])
 def learn(con,tar):
@@ -16,8 +13,7 @@ def learn(con,tar):
         if tar[i] == "YES":
             for x in range(len(sh)):
                 if h[x] != sh[x]:
-                    sh[x] = '?'
-                    gh[x][x] = '?'
+                    sh[x] = gh[x][x] = '?'
         if tar[i] == "NO":
             for x in range(len(sh)):
                 if h[x] != sh[x]:
@@ -27,7 +23,7 @@ def learn(con,tar):
         print("steps of candidate eliminate algorithm",i+1)
         print(sh)
         print(gh)
-    indices=[i for i,val in enumerate(gh)if val == ['?','?','?','?','?','?']]
+    indices=[i for i,val in enumerate(gh) if val == ['?','?','?','?','?','?']]
     for i in indices:
         gh.remove(['?','?','?','?','?','?'])
     return sh,gh
