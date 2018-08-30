3faa144bca1308f0afe03a237ee43e61f4deb37e
Surya Xanden <26842395+SuryaXanden@users.noreply.github.com>
2018-08-30 14:28:56 +0530

    Update pg1.py

--- pg1.py
+++ pg1.py
@@ -6,16 +6,13 @@ with open('p1.csv') as csv_file:
     loa = 0
     for row in read_csv:
         if row[len(row)-1].upper()=="YES":
-            loa += 1
             data.append(row)
     var = len(data[0])-1
-    print "The total number of positive training sets: ", var
-    print "The total length of attributes: ", loa
 TotExamples = len(data)
 i=0
 j=0
 k=0
-print('Steps of Find-s Algorithm :\n')
+print('Steps of Find-s Algorithm :')
 list = [] 
 p=0
 d = len(data[p])-1;
@@ -30,9 +27,9 @@ for i in range(TotExamples):
             k += 1
         else:
             hypo[k]
-        print(hypo)
+        #print(hypo)#dataset
 i += 1
-print('\n Max Specific Find-S hypothesis for given csv are :')
+print('Max Specific Find-S hypothesis for given csv are:')
 list = []
 for i in range(d):
     list.append(hypo[i])
